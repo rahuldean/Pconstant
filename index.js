986c9eec2c3904c8027fbf15e07e798a14e5d25b
Rahul Reddy <godhc@me.com>
2015-01-13 02:41:06 +0530

    openshift environment variables for express

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var express               = require('express'),
 // Set static files location
 app.use(express.static(__dirname + '/public'));
 
-var server                = app.listen(process.env.PORT || 8000, process.env.IP || '0.0.0.0', function(){
+var server                = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8000, process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0', function(){
     console.log('Listening on %s at %s', server.address().port, server.address().address);
 
     // Register routes
